12e01566b6d02145cb04ed91dc1ce60701f610e9
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:19:06 -0700

    Rename instance variable to mac_and_cheese and add test for add_ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -6,16 +6,28 @@ require './lib/recipe'
 class RecipeTest < Minitest::Test
 
   def setup
-    @recipe = Recipe.new("Mac and Cheese")
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
   end
 
   def test_it_exists
-    assert_instance_of Recipe, @recipe
+    assert_instance_of Recipe, @mac_and_cheese
   end
 
   def test_it_initializes
     expected = {}
-    assert_equal "Mac and Cheese", @recipe.name
-    assert_equal expected, @recipe.ingredients_required
+    assert_equal "Mac and Cheese", @mac_and_cheese.name
+    assert_equal expected, @mac_and_cheese.ingredients_required
   end
+
+  def test_it_can_add_ingredients
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    expected = {@cheese => 2, @mac => 8}
+
+    assert_equal expected, @mac_and_cheese.ingredients_required
+  end
+
 end
