78fdd048c26e7de0e4db3d8aa26edce10ba4a3fe
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:58:25 -0700

    Create cookbook test with no assertions yet

---
+++ test/cookbook_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/pantry'
+require './lib/cookbook'
+
+class CookBookTest < Minitest::Test
+
+  def setup
+
+  end
+
+  def test_it_exists
+
+  end
+end
