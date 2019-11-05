5649aab05c7bb6fb230ed3ccb1749969217ffa8c
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:59:28 -0700

    Create exists test for cookbook, create cookbook class

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -8,10 +8,10 @@ require './lib/cookbook'
 class CookBookTest < Minitest::Test
 
   def setup
-
+    @cookbook = CookBook.new
   end
 
   def test_it_exists
-
+    assert_instance_of CookBook, @cookbook
   end
 end
