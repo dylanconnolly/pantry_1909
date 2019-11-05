cc9ec5efb4be8331fa113e0c9b54bcc1f4cb876c
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:41:22 -0700

    Change stock attribut to be hash with default value 0, write method for stock_check method and add test for method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,16 +8,27 @@ class PantryTest < Minitest::Test
 
   def setup
     @pantry = Pantry.new
+
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    mac = Ingredient.new("Macaroni", "oz", 200)
+    mac_and_cheese = Recipe.new("Mac and Cheese")
+    mac_and_cheese.add_ingredient(@cheese, 2)
+    mac_and_cheese.add_ingredient(mac, 8)
   end
 
   def test_it_exists
     assert_instance_of Pantry, @pantry
   end
 
-  def test_it_initializes_with_empty_stock
+  def test_it_initializes_with_empty_stock_and_default_value_is_0
     expected = {}
 
     assert_equal expected, @pantry.stock
+    assert_equal 0, @pantry.stock["Made up ingredient"]
+  end
+
+  def test_it_can_check_stock_for_amount_of_ingredient
+    assert_equal 0, @pantry.stock_check(@cheese)
   end
 
 end
