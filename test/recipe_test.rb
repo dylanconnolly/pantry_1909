19140d5ec78bc1730d9eb8eae134f4656a807ebe
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:10:21 -0700

    Create recipe file and recipe test file. Write test for exists

---
+++ test/recipe_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def setup
+    @recipe = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @recipe
+  end
+end
