65867cd6f32cd7d9480c4eac71756b3394ecedc0
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-05 09:46:02 -0700

    Create test for enough_ingredients_for? method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -10,10 +10,10 @@ class PantryTest < Minitest::Test
     @pantry = Pantry.new
 
     @cheese = Ingredient.new("Cheese", "C", 50)
-    mac = Ingredient.new("Macaroni", "oz", 200)
-    mac_and_cheese = Recipe.new("Mac and Cheese")
-    mac_and_cheese.add_ingredient(@cheese, 2)
-    mac_and_cheese.add_ingredient(mac, 8)
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
   end
 
   def test_it_exists
@@ -39,4 +39,11 @@ class PantryTest < Minitest::Test
     @pantry.restock(@cheese, 10)
 
     assert_equal 15, @pantry.stock_check(@cheese)
+  end
+
+  def test_enough_ingredients_for
+    @pantry.restock(@cheese, 5)
+
+    assert_equal false, @pantry.enough_ingredients_for?(@mac_and_cheese)
+  end
 end
